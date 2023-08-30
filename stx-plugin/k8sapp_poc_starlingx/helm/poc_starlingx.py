5496df423c947f88861d2257303e4d9354c3d145
Bruno Muniz <bruno.muniz@encora.com>
2023-08-30 15:06:06 -0300

    Change inheritance from BaseHelm to FluxCDBaseHelm in stx-plugin

--- stx-plugin/k8sapp_poc_starlingx/helm/poc_starlingx.py
+++ stx-plugin/k8sapp_poc_starlingx/helm/poc_starlingx.py
@@ -3,9 +3,10 @@ from k8sapp_poc_starlingx.common import constants
 from sysinv.helm import base
 from sysinv.common import exception
 
-class PocStarlingxHelm(base.BaseHelm):
+
+class PocStarlingxHelm(base.FluxCDBaseHelm):
     """Class to encapsulate helm operations for the poc chart"""
-    
+
     SUPPORTED_NAMESPACES = base.BaseHelm.SUPPORTED_NAMESPACES + \
         [constants.HELM_NS_POC_STARLINGX]
     SUPPORTED_APP_NAMESPACES = {
@@ -13,10 +14,10 @@ class PocStarlingxHelm(base.BaseHelm):
     }
 
     CHART = constants.HELM_CHART_POC_STARLINGX
+    HELM_RELEASE = constants.FLUXCD_HELMRELEASE_POC_STARLINGX
 
     def get_namespaces(self):
         return self.SUPPORTED_NAMESPACES
-
 
     def get_overrides(self, namespace=None):
 
